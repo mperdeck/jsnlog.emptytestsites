cb3332371d961e3b9869d7f96de743dcf33e43bd
mperdeck <mperdeck@gmail.com>
2015-01-21 21:57:11 +1100

    Added common logging empty site for common logging 3

---
+++ EmptyTestSites/EmptyCommonLogging3Nlog/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace EmptyCommonLogging2Nlog
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ EmptyTestSites/EmptyCommonLogging3Nlog/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Common.Logging;
+
+namespace EmptyNLog.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ILog log = LogManager.GetLogger("Server Logger");
+            log.Debug("Warn Message generated on server");
+
+            return View();
+        }
+    }
+}
